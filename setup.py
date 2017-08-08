0b8bd53161ac1f8c3aa4b18c5c0fbb623b67df43
David Fialho <fialho.david@gmail.com>
2017-08-08 18:48:29 +0100

    Renamed aethelometer to aethalometer

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='aethelometer_collector',
+    name='aethalometer_collector',
     version='0.1',
-    description='Data collector for the aethelometer device',
-    url='https://github.com/pico-collectors/aethelometer_collector.git',
+    description='Data collector for the aethalometer device',
+    url='https://github.com/pico-collectors/aethalometer_collector.git',
     license='MIT',
     author='David Fialho',
     author_email='fialho.david@protonmail.com',
